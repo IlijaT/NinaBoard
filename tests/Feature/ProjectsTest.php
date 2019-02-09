9418c800a7fca6a431f29d2ef46e65039586aaf0
IlijaT <tatalovicilija@gmail.com>
2019-02-09 16:42:09 +0100

    TDD project add to database

---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProjectsTest extends TestCase
+{
+    use WithFaker, RefreshDatabase;
+
+    
+    /** @test */
+    public function a_user_can_create_a_project()
+    {
+        $this->withoutExceptionHandling();
+
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph
+        ];
+
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
+
+        $this->assertDatabaseHas('projects', $attributes);
+
+        $this->get('/projects')->assertSee($attributes['title']);
+    }
+
+    /** @test */
+    public function a_project_requires_a_title()
+    {
+        $project = $user = factory(\App\Project::class)->raw([
+            'title' => '',
+        ]);
+
+        $this->post('/projects',  $project)->assertSessionHasErrors('title');
+    }
+
+    /** @test */
+    public function a_project_requires_a_description()
+    {
+        $project = $user = factory(\App\Project::class)->raw([
+            'description' => '',
+        ]);
+
+        $this->post('/projects',  $project)->assertSessionHasErrors('description');
+    }
+}
