919358309f531866c75fb758c3fd73a513dd92f9
IlijaT <tatalovicilija@gmail.com>
2019-03-26 13:59:20 +0100

    Login blade changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,18 +2,18 @@
 
  @section('content')
        
-    <div class="  col-md-8">
+    <div class="col-lg-4">
         <div class="card">
-            <div class="card-header">{{ __('Login') }}</div>
+            <div class="card-header text-lg text-center">{{ __('Login') }}</div>
 
             <div class="card-body">
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
 
-                    <div class="form-group row">
-                        <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                    <div class="form-group">
+                        <label for="email">{{ __('E-Mail Address') }}</label>
 
-                        <div class="col-md-6">
+                        <div>
                             <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
                             @if ($errors->has('email'))
@@ -24,10 +24,10 @@
                         </div>
                     </div>
 
-                    <div class="form-group row">
-                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                    <div class="form-group">
+                        <label for="password" class="col-form-label">{{ __('Password') }}</label>
 
-                        <div class="col-md-6">
+                        <div>
                             <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                             @if ($errors->has('password'))
@@ -38,8 +38,8 @@
                         </div>
                     </div>
 
-                    <div class="form-group row">
-                        <div class="col-md-6 offset-md-4">
+                    <div class="form-group">
+                        <div>
                             <div class="form-check">
                                 <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -50,14 +50,14 @@
                         </div>
                     </div>
 
-                    <div class="form-group row mb-0">
-                        <div class="col-md-8 offset-md-4">
-                            <button type="submit" class="btn py-1 px-4 bg-green text-normal button rounded-lg text-white is-link hover:bg-green-dark">
+                    <div class="form-group">
+                        <div class="flex flex-column">
+                            <button type="submit" class="btn py-1 px-4 bg-green text-lg button rounded-lg text-white is-link hover:bg-green-dark">
                                 {{ __('Login') }}
                             </button>
 
                             @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                <a class="mt-2" href="{{ route('password.request') }}">
                                     {{ __('Forgot Your Password?') }}
                                 </a>
                             @endif
